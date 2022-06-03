720676c82bd80ef766c5d86e0e6e3bcf3323aabb
caiqueoak03 <caique.silva@sptech.school>
2022-06-03 01:28:58 -0300

    tela de perfil e função do registro box

---
+++ public/js/perfil.js
@@ -0,0 +1,13 @@
+function showBoxRegistro() {
+	const boxAtivo = registros_wrapper.style.display == "flex";
+
+	if (boxAtivo) {
+		registros_wrapper.style.display = "none";
+	} else {
+		registros_wrapper.style.display = "flex";
+	}
+}
+
+function cadastrarRegistro() {
+  
+}
